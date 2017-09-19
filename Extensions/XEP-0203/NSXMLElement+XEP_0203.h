e36c7121b938cea84a3085c949148f8b40687824
Piotr Wegrzynek <pwe@trifork.com>
2017-09-19 11:20:51 +0200

    Methods for extracting delayed delivery origin JID/reason description from an XMPP message

--- Extensions/XEP-0203/NSXMLElement+XEP_0203.h
+++ Extensions/XEP-0203/NSXMLElement+XEP_0203.h
@@ -1,10 +1,13 @@
 #import <Foundation/Foundation.h>
 @import KissXML;
 
+@class XMPPJID;
 
 @interface NSXMLElement (XEP_0203)
 
 - (BOOL)wasDelayed;
 - (NSDate *)delayedDeliveryDate;
+- (XMPPJID *)delayOriginJID;
+- (NSString *)delayReasonDescription;
 
 @end
